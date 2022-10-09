89decfb599c7060bcaf93b47963bd37daf7ddb49
Erin Brown <erinbrown013@gmail.com>
2022-10-09 12:45:39 -0400

    PPP-57 Add JokeOwner to Joke class

--- JokesWebApp/Models/Joke.cs
+++ JokesWebApp/Models/Joke.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string JokeQuestion { get; set; }
         public string JokeAnswer { get; set; }
+        public string JokeOwner { get; set; }
 
 
         public Joke()
